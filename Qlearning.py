a6de5cfab95d6baa9bb987ead6db81d258fd3425
CDU55 <48677296+CDU55@users.noreply.github.com>
2019-12-02 20:16:52 +0200

    Added valid actions function

--- Qlearning.py
+++ Qlearning.py
@@ -8,7 +8,6 @@ def create_qtable(matrix, obstacles, endpoints):
             Qtable[(i, j)] = dict()
             for action in possible_moves:
                 next_state = (i + action[0], j + action[1])
-                print(next_state)
                 if next_state[0] < 0 or next_state[1] < 0 or next_state[1] >= len(matrix) or next_state[1] >= len(
                         matrix):
                     Qtable[(i, j)][action] = None
@@ -18,3 +17,14 @@ def create_qtable(matrix, obstacles, endpoints):
                     Qtable[(i, j)][action] = 1
                 else:
                     Qtable[(i, j)][action] = -0.004
+    return Qtable
+
+
+def get_valid_actions(state, Qtable):
+    valid_actions = list(filter(lambda x: Qtable[state][x] is not None, Qtable[state].keys()))
+    return valid_actions
+
+
+table = create_qtable([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [(1, 1)], [(2, 2)])
+print(table)
+print(get_valid_actions((0, 0), table))
