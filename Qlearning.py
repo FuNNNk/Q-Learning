641446cf0a55bca833e78329f0d4c72119691062
CDU55 <48677296+CDU55@users.noreply.github.com>
2019-12-04 15:29:36 +0200

    Stable version

--- Qlearning.py
+++ Qlearning.py
@@ -2,7 +2,7 @@ import random
 
 possible_moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 LEARNING_RATE = 0.2
-GAMMA = 0
+GAMMA = 0.2
 EPSILON = 0.1
 EPISODES = 2000
 
@@ -20,9 +20,9 @@ def create_qtable(matrix, obstacles, endpoints):
                 elif next_state in obstacles:
                     Qtable[(i, j)][action] = -1000
                 elif next_state in endpoints:
-                    Qtable[(i, j)][action] = 1000
+                    Qtable[(i, j)][action] = 10
                 else:
-                    Qtable[(i, j)][action] = -0.04
+                    Qtable[(i, j)][action] = 0
     return Qtable
 
 
@@ -31,15 +31,18 @@ def get_valid_actions(state, Qtable):
     return valid_actions
 
 
-def get_max_key(state, Qtable):
-    pass
-
-
-game_table = [[0, 0, 0], [0, 0, 0], [0, 0, 1]]
-obstacles = []
-endpoints = [(2, 2)]
-table = create_qtable(game_table, obstacles, endpoints)
-
+def create_matrix(lines, columns, endpoints, obstacles):
+    matrix = list()
+    for i in range(lines):
+        matrix.append(list())
+        for j in range(columns):
+            if (i, j) in endpoints:
+                matrix[i].append(10)
+            elif (i, j) in obstacles:
+                matrix[i].append(-1000)
+            else:
+                matrix[i].append(0)
+    return matrix
 
 
 def possible_states(matrix):
@@ -73,38 +76,58 @@ def take_action(matrix, state, action):
     if new_state[0] < 0 or new_state[1] < 0 or new_state[0] >= len(matrix) or new_state[1] >= len(matrix):
         reward = -1000
         end_game = True
-        return reward, state, end_game
+        return reward, new_state, end_game
     else:
-        reward = table[state][action]
-    if matrix[new_state[0]][new_state[1]] == 1000:
+        reward = matrix[new_state[0]][new_state[1]]
+    if matrix[new_state[0]][new_state[1]] == 10:
         end_game = True
     return reward, new_state, end_game
 
 
-all_states = possible_states(game_table)
-
-
-def train():
-    for _ in range(EPISODES):
+def train(game_table, table):
+    for _ in range(500):
         end_of_game = False
-        current_state = random.choice(all_states)
+        current_state = random.choice(possible_states(game_table))
+        next_state = current_state
         reward = 0
         while not end_of_game:
+            current_state = next_state
             valid = get_valid_actions(current_state, table)
             max_reward_actions = max_value_actions(current_state, table)
-            max_reward_action = tuple()
             max_reward_action = random.choice(max_reward_actions)
             if random.random() < EPSILON:
                 max_reward_action = random.choice(valid)
-            reward, current_state, end_of_game = take_action(game_table, current_state, max_reward_action)
+            reward, next_state, end_of_game = take_action(game_table, current_state, max_reward_action)
             if end_of_game:
                 table[current_state][max_reward_action] = reward
             else:
-                optimal_future_value = 0
+                optimal_future_value = table[next_state][random.choice(max_value_actions(next_state, table))]
                 discounted_optimal_future_value = GAMMA * optimal_future_value
                 learned_value = reward + discounted_optimal_future_value
-                table[current_state][max_reward_action] = (1 - LEARNING_RATE) * table[current_state][max_reward_action] + LEARNING_RATE * learned_value
+                table[current_state][max_reward_action] = (1 - LEARNING_RATE) * table[current_state][
+                    max_reward_action] + LEARNING_RATE * learned_value
 
 
-train()
-print(table)
+def test(game_table, table):
+    end_game = False
+    path = list()
+    current_state = (0, 0)
+    next_state = current_state
+    while not end_game:
+        current_state = next_state
+        valid = get_valid_actions(current_state, table)
+        max_reward_actions = max_value_actions(current_state, table)
+        max_reward_action = random.choice(max_reward_actions)
+        reward, next_state, end_game = take_action(game_table, current_state, max_reward_action)
+        print(next_state)
+        path.append(next_state)
+
+
+def create_matrix_table_and_train(lines, columns, endpoints, obstacles):
+    matrix = create_matrix(lines, columns, endpoints, obstacles)
+    table = create_qtable(matrix, obstacles, endpoints)
+    train(matrix, table)
+    test(matrix, table)
+
+
+create_matrix_table_and_train(4, 4, [(3, 3)], [(1, 2), (1, 1), (1, 0)])
